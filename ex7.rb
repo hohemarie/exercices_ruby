451ff9f9db7ab4ae6cbd5d330dfad113b1bd5ba4
hohemarie <marie.goudon@laposte.net>
2018-12-08 17:17:02 +0100

    exercice 7

---
+++ ex7.rb
@@ -0,0 +1,10 @@
+
+
+s=[true, true, true, false, true, true, true, true , true, false, true, false, true, false, false, true , true, true, true, true , false, false, true, true]
+
+
+
+def functionname(variable)
+   puts variable.count(true) 
+end
+functionname(s)
