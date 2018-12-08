ec78ac8ebd72c661df9389d8f7163d033a06685b
hohemarie <marie.goudon@laposte.net>
2018-12-08 16:45:36 +0100

    exercice 6

---
+++ ex6.rb
@@ -0,0 +1,16 @@
+
+def bonjour 
+    puts "Bonjour monde!"
+end
+
+bonjour
+bonjour
+def retourne(array)
+	arr.each { |q| ary.bsearch {|x| x <= q } }
+end
+
+puts "565863,565".to_i
+a = [1,1,2,3,3,4]
+min = a.min
+a.select { |i| i == min }
+puts min
