3188b5ed07f207034dd48de7b9abcd6673c211fd
hohemarie <marie.goudon@laposte.net>
2018-12-10 20:05:29 +0100

    exercice 12 structure globale de lexercice sans loppose du nombre

--- ex12.rb
+++ ex12.rb
@@ -1,10 +1,14 @@
 p=[-9,-18,27,14,-5,-2,1,35,29,-32,61,23,-45,16]
 list=[]
 cnt=0
+a=0
 def invert(array)
-	p.each do |model|
-	a=-(model)
-	list.insert(cnt,a)
-	cnt+=1
+	array.each do |model|
+		puts(model)	
+		#a=-(model.amount)
+		#puts(a)
+		#list  << -(model)
+	end
 end
 invert(p)
+puts("list",list)
