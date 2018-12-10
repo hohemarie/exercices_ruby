29d21c5c87296fd58cf7b7989a777e5943bd4e45
hohemarie <marie.goudon@laposte.net>
2018-12-10 18:34:08 +0100

    exercice 11

---
+++ ex11.rb
@@ -0,0 +1,14 @@
+def summation
+	puts "addition"
+	#nb=gets.chomps
+	#puts(nb)
+	sum =0
+	1.upto(9) do |index|
+		puts(index)
+		sum+=index		
+		puts(sum)
+	end
+end
+summation
+#nb=gets.chomp
+#puts(nb)
