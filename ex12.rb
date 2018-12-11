be02666698e929c8a02a343f9b288be0c706c264
hohemarie <marie.goudon@laposte.net>
2018-12-11 14:56:55 +0100

    exercice 12

--- ex12.rb
+++ ex12.rb
@@ -1,14 +1,17 @@
 p=[-9,-18,27,14,-5,-2,1,35,29,-32,61,23,-45,16]
-list=[]
+list={}
 cnt=0
 a=0
 def invert(array)
 	array.each do |model|
-		puts(model)	
-		#a=-(model.amount)
-		#puts(a)
-		#list  << -(model)
+		#puts(model)	
+		a=-(model)
+		puts(a)
+		#list.push(a) 
+		#list.push(model)
+		#puts(list)
 	end
 end
 invert(p)
 puts("list",list)
+
